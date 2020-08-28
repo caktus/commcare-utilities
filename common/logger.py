968d6edab6eda8768fba4581d478f1e15d28059d
Benjamin White <960351+benjaminEwhite@users.noreply.github.com>
2020-08-28 15:45:31 -0400

    WIP: Refactor into package

--- common/logger.py
+++ common/logger.py
@@ -1,1 +1,22 @@
 import logging
+from logging.handlers import RotatingFileHandler
+import os
+from pathlib import Path
+
+log_format = "[%(asctime)s] [%(levelname)s] - %(message)s"
+
+log_level = os.environ.get("COMMCARE_UTILITIES_LOG_LEVEL", "INFO")
+log_file_path = os.environ.get(
+    "COMMCARE_UTILITIES_LOG_PATH",
+    Path(__file__).parent.absolute().joinpath("..", "logs"),
+)
+log_file_path.mkdir(parents=True, exist_ok=True)
+
+log_file = log_file_path.joinpath("cc-utilities.log")
+
+rfh = RotatingFileHandler(
+    filename=log_file, mode="a", maxBytes=5 * 1024 * 1024, delay=0
+)
+
+logging.basicConfig(level=log_level, format=log_format, handlers=[rfh])
+logger = logging.getLogger("main")
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="commcare_utilities",
+    version="0.1",
+    description="Helpful utilities for working with CommCare",
+    url="https://github.com/caktus/commcare-utilities",
+    author="Benjamin White",
+    author_email="ben@benjamineugenewhite.com",
+    # license="MIT",
+    packages=["common"],
+    install_requires=[
+        "openpyxl",
+        "requests",
+        "commcare-export",
+        "pre-commit",
+        "SQLAlchemy",
+        "phonenumbers",
+        "pandas",
+    ],
+    scripts=["scripts"],
+    zip_safe=False,
+)
