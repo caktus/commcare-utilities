e5672f03b0e6a5fd8a7e0372ecff4865bc2ed3de
Benjamin White <960351+benjaminEwhite@users.noreply.github.com>
2020-09-17 13:23:30 -0400

    only log when env var explicitly set

--- cc_utilities/logger.py
+++ cc_utilities/logger.py
@@ -6,19 +6,8 @@ from pathlib import Path
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-log_file_path_from_env = os.environ.get("COMMCARE_UTILITIES_LOG_PATH")
-log_file_path = (
-    Path(log_file_path_from_env)
-    if log_file_path_from_env
-    else Path(__file__).parent.absolute().joinpath("..", "logs")
-)
-log_file_path.mkdir(parents=True, exist_ok=True)
-log_file = log_file_path.joinpath("cc-utilities.log")
-file_logger = RotatingFileHandler(
-    filename=log_file, mode="a", maxBytes=5 * 1024 * 1024, delay=0
-)
-file_logger.setLevel(logging.WARNING)
-file_logger.setFormatter(formatter)
+CONSOLE_LOGGER_LEVEL = "INFO"
+FILE_LOGGER_LEVEL = "WARNING"
 
 console_logger = logging.StreamHandler(sys.stdout)
 console_logger.setLevel(logging.DEBUG)
@@ -26,6 +15,17 @@ console_logger.setFormatter(formatter)
 
 
 logger = logging.getLogger("main")
-logger.setLevel(logging.INFO)
+logger.setLevel(getattr(logging, CONSOLE_LOGGER_LEVEL))
 logger.addHandler(console_logger)
-logger.addHandler(file_logger)
+log_file_path_from_env = os.environ.get("COMMCARE_UTILITIES_LOG_PATH")
+
+if log_file_path_from_env:
+    log_file_path = Path(log_file_path_from_env)
+    log_file_path.mkdir(parents=True, exist_ok=True)
+    log_file = log_file_path.joinpath("cc-utilities.log")
+    file_logger = RotatingFileHandler(
+        filename=log_file, mode="a", maxBytes=5 * 1024 * 1024, delay=0
+    )
+    file_logger.setLevel(getattr(logging, FILE_LOGGER_LEVEL))
+    file_logger.setFormatter(formatter)
+    logger.addHandler(file_logger)
