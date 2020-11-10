c828a896a82fefca5dc322181380b2306aeff043
Benjamin White <960351+benjaminEwhite@users.noreply.github.com>
2020-11-10 11:50:38 -0500

    reassign to var

--- cc_utilities/legacy_upload.py
+++ cc_utilities/legacy_upload.py
@@ -100,7 +100,7 @@ def clean_raw_case_data_df(df, data_dict):
 
     def _remove_chars(val, removable_chars=removeable_chars):
         for char in removable_chars:
-            val.replace(char, "")
+            val = val.replace(char, "")
         return val
 
     for col in cleanup_cols:
@@ -399,10 +399,15 @@ def validate_multi_select_field(raw_value, allowed_values):
 def validate_phone_number_field(raw_value, country_code="US"):
     "Validate a value whose CommCare data type is `phone_number`"
     # if this field is blank we just treat it as valid
-    if raw_value in EMPTY_PHONE_VALUES:
-        return True
-    number = phonenumbers.parse(raw_value, country_code)
-    return phonenumbers.is_valid_number(number)
+    try:
+        if raw_value in EMPTY_PHONE_VALUES:
+            return True
+        number = phonenumbers.parse(raw_value, country_code)
+        return phonenumbers.is_valid_number(number)
+    except Exception:
+        import pdb
+
+        pdb.set_trace()
 
 
 def normalize_phone_number(raw_value, col_name=None, country_code="US"):
